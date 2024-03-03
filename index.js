c3e8bb671a81e41909dd1abfe464d52c51c45a18
dwi-puspita <51149174+dwipuspit@users.noreply.github.com>
2024-03-03 23:25:40 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 require("dotenv").config();
+const axios = require("axios").default;
+TOKEN = process.env.KEY;
 const readline = require('node:readline').createInterface({
     input: process.stdin,
     output: process.stdout,
 });
-TOKEN = process.env.KEY;
 
 readline.question("Input Address: ", address => {
     checkBalance(address);
     readline.close();
 })
-export async function checkBalance(address) {
+async function checkBalance(address) {
     try {
-        const res = await fetch("https://api.etherscan.io/api?module=account&action=balance&address=" + address + "&tag=latest&apikey=" + TOKEN);
-        if (!res.ok) throw Error("getting error")
-        const data = await res.json();
+        const res = await axios.get("https://api.etherscan.io/api?module=account&action=balance&address=" + address + "&tag=latest&apikey=" + TOKEN).then(res => res);
+        const data = await res.data;
         const getWei = data.result;
         data.result = weiToETH(getWei);
         return console.log("Your balance: " + data.result + " ETH")
@@ -24,3 +24,5 @@ export async function checkBalance(address) {
 function weiToETH(balance) {
     return balance / 1000000000000000000; // 1 Eth
 }
+
+module.exports = { readline, checkBalance }
